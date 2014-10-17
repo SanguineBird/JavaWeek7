7814225437a55a842b3050489a7b1845815a4972
SanguineBird <SanguineBird@users.noreply.github.com>
2014-10-17 12:27:54 -0400

    Create CounterView.java

---
+++ CounterView.java
@@ -0,0 +1,12 @@
+/**Class: CIT-130-BIN1
+Name: Meghan Moore
+Date: 10/18/2014
+Programming Assignment 7 - Part 1 - CounterView class
+Purpose: Implement the provided UML for a counter that increments
+once a second until a given number of seconds is reached.*/
+
+public class CounterView{
+  public void Display(int counterValue){
+    System.out.println((toString.counterValue + "...");
+  }
+}
