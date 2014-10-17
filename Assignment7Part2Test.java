c908f3338dc6866bf3748f40ead7b8d507f8695a
SanguineBird <SanguineBird@users.noreply.github.com>
2014-10-17 15:22:00 -0400

    Update Assignment7Part2Test.java
    This compiles.

--- Assignment7Part2Test.java
+++ Assignment7Part2Test.java
@@ -10,17 +10,17 @@ import java.util.Arrays;
 public class Assignment7Part2Test{
   
   public static void main(String[] args){
-    BinarySearch searches = new BinarySearch;
+    BinarySearch searches = new BinarySearch();
     int result, key;
-    int [] intArray = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
+    Integer [] integerArray = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
     String [] stringArray = {"apples", "oranges", "peaches", "strawberries", "watermelons"};
     
     System.out.println("Integer test array contains:");
-    System.out.println(Arrays.toString(intArray));
+    System.out.println(Arrays.toString(integerArray));
     
-    for(key = -3, key = 4, key++){
-      result = searches.<Integer>search(intArray, 0, 10, key);
-      searches.toString(key, result);
+    for(key = -3; key == 4; key++){
+      result = searches.<Integer>search(integerArray, 0, 10, key);
+      searches.toString(Integer.toString(key), result);
     }
     
     System.out.println("\nString test array contains:");
@@ -31,6 +31,6 @@ public class Assignment7Part2Test{
     result = searches.<String>search(stringArray, 0, 10, "plums");
     searches.toString("plums", result);
     
-    System.out.println("\nProcess completed.")
+    System.out.println("\nProcess completed.");
   }
 }
